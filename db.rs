a426f9e3290f0cc90e76ef3cef3eba6560943ea5
MisterBottom-png <badassgang@gmail.com>
2026-01-02 16:37:19 +0200

    fix: correct SQL parameter count in upsert_photo function

--- db.rs
+++ db.rs
@@ -190,7 +190,7 @@ pub fn upsert_photo(conn: &DbConnection, photo: &PhotoRecord) -> Result<i64> {
             last_modified
         )
         VALUES (
-            ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19, ?20, ?21, ?22, ?23,
+            ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19, ?20, ?21, ?22,
             strftime('%s','now'),
             strftime('%s','now'),
             strftime('%s','now')
